c7668c27534f0e511b5ef25948eb419e407cf24c
Evie Ciobanu <me@eevie.ro>
2021-12-20 10:57:36 +0200

    update

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
 # Configure your own Haskell nix shell!
 let
   # Do not touch this part.
-  tooling = import ./nix;
-  pkgs = tooling.pkgs;
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs {};
 
   # TODO: Test dev.sh and add deps as needed.
   # You can remove anything from here if you'd rather manage it.
@@ -32,34 +32,18 @@ let
 
   projectDeps = consoleDeps ++ serverDeps ++ testDeps;
 
-  # Change GHC version here. If unsure about what's available, open a
-  # shell and run `nix repl`:
-  #
-  # $ nix repl
-  # nix-repl> tooling = import ./nix
-  # nix-repl> tooling.haskell
-  # { ghc8102 = { ... }; ghc865 = { ... }; ghc844 = { ... }; }
-  #
-  # You can pick any of the above.
-  self = tooling.haskell.ghc8102;
-
-  # inputs is used below
-  inputs = self.packages;
-
   # Common tooling used by the whole team/CI. Remove if you'd rather
   # use the system-wide version you might already have.
   commonTooling =
-    [ self.compiler # GHC version as defined above at 'self'
-      inputs.stylish-haskell
-      inputs.hlint
-      inputs.cabal-install
+    [ pkgs.haskell.compiler.ghc8107 # GHC version as defined above at 'self'
+      pkgs.haskell.packages.ghc8107.cabal-install
     ];
 
   # Personal tooling: change/update as needed, e.g. `inputs.ghcide`.
   personalTooling =
-    [ inputs.haskell-language-server
-      inputs.cabal2nix
-      inputs.implicit-hie
+    [ pkgs.haskell.packages.ghc8107.haskell-language-server
+      pkgs.haskell.packages.ghc8107.cabal2nix
+      pkgs.haskell.packages.ghc8107.implicit-hie
     ];
 in
   pkgs.mkShell {
