385cb3151cad728cfa7f8adc23b95dfdcd9142e8
Evie Ciobanu <me@eevie.ro>
2022-01-28 22:43:30 +0200

    update

--- shell.nix
+++ shell.nix
@@ -2,33 +2,35 @@
 let
   # Do not touch this part.
   sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs {};
+  pkgs = import sources.nixpkgs { };
 
   # TODO: Test dev.sh and add deps as needed.
   # You can remove anything from here if you'd rather manage it.
   consoleDeps =
-    [ pkgs.nodejs
+    [
+      pkgs.nodejs
       pkgs.google-cloud-sdk
     ];
 
   serverDeps =
-    [ pkgs.zlib
+    [
+      pkgs.zlib.dev
       pkgs.postgresql
       pkgs.libkrb5
       pkgs.openssl
       pkgs.freetds
       pkgs.unixODBC
       pkgs.unixODBCDrivers.msodbcsql17
-      pkgs.pcre
       pkgs.pcre.dev
-      pkgs.zstd
       pkgs.zstd.dev
       pkgs.libmysqlclient
       pkgs.docker-compose
     ];
 
   testDeps =
-    [ pkgs.python3
+    [
+      pkgs.python3
+      pkgs.libffi
     ];
 
   projectDeps = consoleDeps ++ serverDeps ++ testDeps;
@@ -36,24 +38,27 @@ let
   # Common tooling used by the whole team/CI. Remove if you'd rather
   # use the system-wide version you might already have.
   commonTooling =
-    [ pkgs.haskell.compiler.ghc8107 # GHC version as defined above at 'self'
+    [
+      pkgs.haskell.compiler.ghc8107 # GHC version as defined above at 'self'
       pkgs.haskell.packages.ghc8107.cabal-install
     ];
 
   # Personal tooling: change/update as needed, e.g. `inputs.ghcide`.
   personalTooling =
-    [ pkgs.haskell.packages.ghc8107.haskell-language-server
+    [
+      pkgs.haskell.packages.ghc8107.haskell-language-server
       pkgs.haskell.packages.ghc8107.cabal2nix
       pkgs.haskell.packages.ghc8107.implicit-hie
+      pkgs.ghcid
     ];
 in
-  pkgs.mkShell {
-    buildInputs = projectDeps ++ commonTooling ++ personalTooling;
+pkgs.mkShell {
+  buildInputs = projectDeps ++ commonTooling ++ personalTooling;
 
-    # This is required for the database scripts to work.
-    # Remove if not wanted.
-    inherit
-      (import ./exports.nix)
-      PGHOST PGPORT PGDATABASE PGUSER PGPASSWORD DATABASE_URL;
-    LD_LIBRARY_PATH="${pkgs.unixODBC}/lib:${pkgs.zlib}/lib:${pkgs.pcre.out}/lib:${pkgs.zstd.out}/lib";
-  }
+  # This is required for the database scripts to work.
+  # Remove if not wanted.
+  inherit
+    (import ./exports.nix)
+    PGHOST PGPORT PGDATABASE PGUSER PGPASSWORD DATABASE_URL;
+  LD_LIBRARY_PATH="${pkgs.unixODBC}/lib:${pkgs.zlib}/lib:${pkgs.pcre.out}/lib:${pkgs.zstd.out}/lib";
+}
