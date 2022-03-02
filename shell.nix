edb96caedae8d63899efea21c7dc25125bda7d4b
Evie Ciobanu <me@eevie.ro>
2022-03-03 00:18:53 +0200

    simplify root

--- shell.nix
+++ shell.nix
@@ -3,6 +3,7 @@ let
   # Do not touch this part.
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs { };
+  common = import ../dotfiles/dev-shell/haskell;
 
   # TODO: Test dev.sh and add deps as needed.
   # You can remove anything from here if you'd rather manage it.
@@ -34,31 +35,14 @@ let
     ];
 
   projectDeps = consoleDeps ++ serverDeps ++ testDeps;
-
-  # Common tooling used by the whole team/CI. Remove if you'd rather
-  # use the system-wide version you might already have.
-  commonTooling =
-    [
-      pkgs.haskell.compiler.ghc8107 # GHC version as defined above at 'self'
-      pkgs.haskell.packages.ghc8107.cabal-install
-    ];
-
-  # Personal tooling: change/update as needed, e.g. `inputs.ghcide`.
-  personalTooling =
-    [
-      pkgs.haskell.packages.ghc8107.haskell-language-server
-      pkgs.haskell.packages.ghc8107.cabal2nix
-      pkgs.haskell.packages.ghc8107.implicit-hie
-      pkgs.ghcid
-    ];
 in
 pkgs.mkShell {
-  buildInputs = projectDeps ++ commonTooling ++ personalTooling;
+  buildInputs = projectDeps ++ common.buildInputs;
 
   # This is required for the database scripts to work.
   # Remove if not wanted.
   inherit
     (import ./exports.nix)
     PGHOST PGPORT PGDATABASE PGUSER PGPASSWORD DATABASE_URL;
-  LD_LIBRARY_PATH="${pkgs.unixODBC}/lib:${pkgs.zlib}/lib:${pkgs.pcre.out}/lib:${pkgs.zstd.out}/lib";
+  LD_LIBRARY_PATH="${pkgs.unixODBC}/lib:${pkgs.zlib}/lib:${pkgs.pcre.out}/lib:${pkgs.zstd.out}/lib:${pkgs.openssl}/lib;";
 }
